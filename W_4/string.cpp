db4f96a5656d7a99c1a77b2851feddb006683fbe
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-05-28 17:30:18 +0800

    just commit

--- W_4/string.cpp
+++ W_4/string.cpp
@@ -1,7 +1,40 @@
+#include <iostream>
 #include <string>
 
 int main(){
+    // >>>
+    // std::cout << "Enter your full name: ";
+    // std::string name;
+    // std::cin >> name;   // this won't work as expected since std::cin breaks on whitespace
+
+    // std::cout << "Enter your age: ";
+    // std::string age;
+    // std::cin >> age;    //cin won't wait for us to input 'age'
+
+    // std::cout << "Your name is " << name << " and your age is " << age << std::endl;
     
-    
+
+    // >>>
+    // A good way to read a whole line from stdin is std::getline(std::cin, VariableName)
+    std::cout << "Enter your full name: ";
+    std::string name;
+    std::getline(std::cin, name);
+    std::cout << "Your name is: " << name << std::endl;
+
+
+    // >>>
+    //  If reading values with std::cin, it’s a good idea to remove the extraneous newline using std::cin.ignore().
+    std::cout << "Pick 1 or 2: ";
+	int choice { 0 };
+	std::cin >> choice;
+ 
+
+    // >>>
+    std::cout << "Pick 1 or 2: ";
+	int choice { 0 };
+	std::cin >> choice;
+	std::cin.ignore(32767, '\n');   // ignore up to 32767 characters until a \n is removed
+ 
+     
     return 0;
 }
