4545d2470680f3ace1b076c7905e9fc1f01a6a8b
Xu Wang <xuxu.wang@mail.utoronto.ca>
2019-06-05 18:37:46 -0400

    start working on week_6 std::cin and extractions

---
+++ W_5/auto.cpp
@@ -0,0 +1,4 @@
+// Note that this only works when initializing a variable upon creation. 
+// Variables created without initialization values can not use this feature
+
+// Warning: The auto keyword can’t be used with function parameters.
